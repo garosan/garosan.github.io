ca37da01a71773bcd96df64ef8396594c400a8d1
Garo San <garosanchez27@gmail.com>
2019-07-21 20:55:59 -0500

    Completed 1st draft of homepage component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,44 @@
 import React from "react";
+import "./Home.css";
+import profile_pic from "../assets/profile_pic.jpg";
 
 const Home = () => {
   return (
-    <div>
-      <h3>Hello from the Home page!</h3>
+    <div className="container home">
+      <div className="row">
+        <div className="col mx-5">
+          <h1>Hello I'm Edgar!</h1>
+          <br />
+          <p style={{ textAlign: "justify" }}>
+            Software engineer with 3 years experience building websites. Focused
+            on creating engaging digital experiences for a better, more
+            connected world.
+          </p>
+          <p style={{ textAlign: "justify" }}>
+            I'm a disciplined, motivated individual with a self-improvement
+            mentality. When I'm not coding or doing tech, I enjoy reading and
+            learning new stuff. I love hiking, running & hitting the gym. Hate
+            to cook but I'm working on it.
+          </p>
+        </div>
+        <div className="col" style={{ textAlign: "center" }}>
+          <img className="profile_pic" src={profile_pic} />
+          <div id="social_handles">
+            <a href="https://www.github.com/garosan/" target="_blank">
+              <i class="fa fa-github fa-2x" />
+            </a>
+            <a href="https://www.instagram.com/garosan1/" target="_blank">
+              <i class="fa fa-camera-retro fa-2x" />
+            </a>
+            <a href="https://www.twitter.com/garosan1/" target="_blank">
+              <i class="fa fa-twitter-square fa-2x" />
+            </a>
+            <a href="mailto:garosanchez27@gmail.com">
+              <i class="fa fa-envelope fa-2x" />
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Projects = () => {
   return (
-    <div>
+    <div className="container">
       <h3>Hello from the Projects page!</h3>
     </div>
   );
