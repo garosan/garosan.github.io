3390d738bfbd02bafe1118dcc771b1f4471beb51
Garo San <garosanchez27@gmail.com>
2020-09-14 17:12:49 -0500

    Updates

---
+++ precache-manifest.8493d81811be9c126979fd712dd7988c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b12d5d0f3d3156cb67d2f2b5cbc8818",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a35317640677005021a",
+    "url": "/static/css/main.946a7e2e.chunk.css"
+  },
+  {
+    "revision": "5db7a3fb75fd1ac9c52b",
+    "url": "/static/js/2.fb2d314b.chunk.js"
+  },
+  {
+    "revision": "3a35317640677005021a",
+    "url": "/static/js/main.f2c985a6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
